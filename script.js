a33c5cb9f36802a54507b381d22719104ed7ddcc
thanosbalo <thanosbalogiannis@gmail.com>
2022-11-13 23:57:37 +0200

    finished submenu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.scrollY;
 
 window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector("#navbar").style.top = "0";
     document.querySelector("#navbar").style.backgroundColor="white";
@@ -11,4 +11,17 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
   if (prevScrollpos==0)
     document.querySelector("#navbar").style.backgroundColor="transparent";
-}
+};
+let subMenu=document.querySelector(".sub-menu");
+let subDisplay=window.getComputedStyle(subMenu);
+let shopBtn=document.querySelector("#shop-btn");
+shopBtn.addEventListener("mouseover",()=>{
+  if (subDisplay.display=="block"){
+    document.querySelector("#navbar").style.backgroundColor="white"; 
+  }
+})
+shopBtn.addEventListener("mouseout",()=>{
+  if (window.scrollY===0){
+    document.querySelector("#navbar").style.backgroundColor="transparent";}
+})
+
