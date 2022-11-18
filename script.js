91d808426312f72b83aac33195abe5ef4e11e3a0
thanosbalo <thanosbalogiannis@gmail.com>
2022-11-18 17:04:36 +0200

    made item page

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
   if (prevScrollpos==0)
     document.querySelector("#navbar").style.backgroundColor="transparent";
-};
+    stickyFunction();
+  };
 
 
 function mouseOver(){
@@ -22,3 +23,38 @@ function mouseOut(){
     document.querySelector("#navbar").style.backgroundColor="transparent";
   };
 };
+
+let heartBtn=document.querySelector("#hearticon");
+let heartStatus=true;
+  heartBtn.addEventListener("click",()=>{
+  if (heartStatus){
+  document.querySelector(".la-heart").classList.remove("lar");
+  document.querySelector(".la-heart").classList.add("las");
+  heartStatus=false;
+  }
+  else {
+    document.querySelector(".la-heart").classList.remove("las");
+    document.querySelector(".la-heart").classList.add("lar");
+    heartStatus=true;
+  }
+
+})
+
+let itemCard=document.querySelector(".item-info-card");
+let sticky=itemCard.offsetTop;
+console.log(sticky);
+function stickyFunction() {
+  if (((window.pageYOffset)+240)> sticky){
+    itemCard.classList.add("sticky");
+  } else {
+    itemCard.classList.remove("sticky");
+  }
+}
+/* window.addEventListener("scroll",()=>{
+    console.log(33);
+    if (window.pageYOffset > sticky) {
+      itemCard.classList.add("sticky");
+    } else {
+      itemCard.classList.remove("sticky");
+    }
+}); */
