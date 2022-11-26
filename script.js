7fa2983641041f3b49b7178ad15ae9f911dc2d1b
thanosbalo <thanosbalogiannis@gmail.com>
2022-11-27 00:25:21 +0200

    finished dresses page

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 window.onscroll = function() {
-  if (window.scrollY > 100) {
+  if (window.scrollY > 50) {
     document.querySelector("#navbar").style.backgroundColor="white";
-  /*   document.querySelector("#navbar").style.position="fixed"; */
   } else {
     document.querySelector("#navbar").style.backgroundColor="transparent";
-    /* document.querySelector("#navbar").style.position="relative"; */
   }
   };
 function mouseOver(){
@@ -15,3 +13,26 @@ function mouseOut(){
     document.querySelector("#navbar").style.backgroundColor="transparent";
   };
 };
+let gridContainer=document.querySelector(".grid-container");
+let twoButton=document.querySelector(".grid-button-2");
+let fourButton=document.querySelector(".grid-button-4");
+let graphBoxTwo=document.querySelectorAll(".graph-box-two");
+let graphBoxFour=document.querySelectorAll(".graph-box-four");
+twoButton.addEventListener("click",()=>{
+  gridContainer.classList.add("grid-two");  
+  graphBoxTwo.forEach((graphBoxTwo) => {
+    graphBoxTwo.classList.add("black-bg");
+  });
+  graphBoxFour.forEach((graphBoxFour)=>{
+    graphBoxFour.classList.remove("black-bg");
+  })
+});
+fourButton.addEventListener("click",()=>{
+  gridContainer.classList.remove("grid-two");  
+  graphBoxFour.forEach((graphBoxFour) => {
+    graphBoxFour.classList.add("black-bg");
+  });
+  graphBoxTwo.forEach((graphBoxTwo)=>{
+    graphBoxTwo.classList.remove("black-bg");
+  });
+});
