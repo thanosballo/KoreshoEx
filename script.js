75ad4c37f71a43d884ae54f42294886ac080b19d
thanosbalo <thanosbalogiannis@gmail.com>
2022-11-26 15:51:46 +0200

    updated grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 window.onscroll = function() {
   if (window.scrollY > 100) {
     document.querySelector("#navbar").style.backgroundColor="white";
+  /*   document.querySelector("#navbar").style.position="fixed"; */
   } else {
     document.querySelector("#navbar").style.backgroundColor="transparent";
+    /* document.querySelector("#navbar").style.position="relative"; */
   }
   };
 function mouseOver(){
