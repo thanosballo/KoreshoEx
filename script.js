f96722ec13e6331e06a313c2327cca0a0687da23
thanosbalo <thanosbalogiannis@gmail.com>
2022-11-29 12:52:45 +0200

    made changes

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-let active=false;
 let navBar=document.querySelector("#navbar");
 let toggleBtn=document.querySelector(".toggle-btn");
 let toggleNavLinks=document.querySelector(".nav-links");
 toggleBtn.addEventListener("click",()=>{
-  if (!navBar.classList.contains("white-bg"))
-  {navBar.classList.add("white-bg");}
-  else if (navBar.classList.contains("white-bg") && (window.scrollY===0)) 
-  { navBar.classList.remove("white-bg");}
-  
   toggleBtn.classList.toggle("open");
   toggleNavLinks.classList.toggle("open");
+  if (toggleNavLinks.classList.contains("open"))
+  {navBar.classList.add("white-bg");}
+  else if (toggleNavLinks.classList.contains("open") && (window.scrollY===0)) 
+  { navBar.classList.remove("white-bg");}
 });
 
 function mouseOver(){
     navBar.classList.add("white-bg");
-    active=true;
 };
 function mouseOut(){
-  if (window.scrollY===0){
+  if (window.scrollY===0 && !toggleNavLinks.classList.contains("open")){
     navBar.classList.remove("white-bg");
-    active=false;
   };
 };
 window.onscroll = function() {
   if (window.scrollY > 0) {
     navBar.classList.add("white-bg");
-    a=true;
-  } else if ((window.scrollY===0) && (active===false)) {
+  } else if (!toggleNavLinks.classList.contains("open") && (window.scrollY===0)) {
     navBar.classList.remove("white-bg");
-    active=false;
   } 
   };
